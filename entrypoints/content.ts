de141cd7f283cfa477f669f7088bf34707c657f2
uncore <contactuncor3@gmail.com>
2025-03-29 10:56:45 +0300

    run the content script at 'document_end' , add new copied message

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -2,6 +2,7 @@ import init from './init';
 
 export default defineContentScript({
   matches: ['https://www.npmjs.com/package/*'],
+  runAt: 'document_end',
   main(ctx) {
     const ui = createIntegratedUi(ctx, {
       position: 'inline',
@@ -12,31 +13,35 @@ export default defineContentScript({
           const div = mainContainer?.children[3].querySelector('h3 + *');
           if (div) {
             init(div as HTMLElement);
-
-            // window.addEventListener('popstate', handler);
-            //react-router does not fire events like popstate,pushstate so navigation events do not work
-            //use a mutation observer instead
-            const observer = new MutationObserver(() => {
-              handler();
-            });
-
-            const config = {
-              childList: true,
-              attributes: true,
-              subtree: true,
-            };
-
-            //h2 > span is the package name
-            const divToObserve = document.querySelector(
-              'main > div > *:nth-child(1) > h2'
-            );
-            observer.observe(divToObserve!, config);
           }
         };
 
         //check the layout of the page
         if (mainContainer && mainContainer.children.length == 4) {
           handler();
+
+          // window.addEventListener('popstate', handler);
+          //react-router does not fire events like popstate,pushstate so navigation events do not work
+          //use a mutation observer instead
+          const observer = new MutationObserver((e) => {
+            handler();
+          });
+
+          const config = {
+            childList: true,
+            attributes: false,
+            subtree: true,
+            characterData: true,
+          };
+          // could observe with the selector below
+          // 'main > div > *:nth-child(1) > h2'
+          //or even this
+          // 'main > div > *:nth-child(1) > h2 > span' //actual package name
+          //but this is more feature proof
+          const divToObserve = document.querySelector(
+            'main > div > *:nth-child(1)',
+          );
+          observer.observe(divToObserve!, config);
         }
       },
     });
--- entrypoints/init.ts
+++ entrypoints/init.ts
@@ -1,53 +1,69 @@
 import { getList } from './popup/utils';
 
-async function init(elementToCopy: HTMLElement) {
-  //leave one element so we can clone the element
-  elementToCopy?.parentElement
-    ?.querySelectorAll('[alt-pkg]')
+async function init(elementToClone: HTMLElement) {
+  elementToClone
+    .parentElement!.querySelectorAll('[alt-pkg]')
     .forEach((_, i) => {
       if (i === 0) return;
       _.remove();
     });
-  const pkg_managers = await getList();
+  const pkgManagers = await getList();
   const pkgName = getPackageName();
-  if (elementToCopy) {
-    let elementToCopyParent = elementToCopy.parentElement;
-    let nextSibling = elementToCopy.nextSibling;
-    elementToCopy.parentNode?.removeChild(elementToCopy);
+  const elementToCopyParent = elementToClone.parentElement;
+  const nextSibling = elementToClone.nextSibling;
+  elementToClone.remove();
 
-    for (let index = 0; index < pkg_managers.length; index++) {
-      const manager = pkg_managers[index];
-      const newClone = elementToCopy.cloneNode(true) as HTMLElement;
-      newClone.setAttribute('alt-pkg', '');
+  for (const manager of pkgManagers) {
+    const newClone = elementToClone.cloneNode(true) as HTMLElement;
+    newClone.setAttribute('alt-pkg', '');
+    newClone.style.position = 'relative';
+    const code = newClone.querySelector('p > code')!;
+    const btn = newClone.querySelector('p > button')!;
 
-      const code = newClone.querySelector('p > code');
-      const btn = newClone.querySelector('p > button');
+    const text = `${manager.name} ${manager.command} ${pkgName}`;
+    btn.addEventListener('click', () => copyToClipboard(newClone, text));
 
-      if (!code || !btn) continue;
-      const text = `${manager.name} ${manager.command} ${pkgName}`;
-      btn.addEventListener('click', () => {
-        try {
-          navigator.clipboard.writeText(text);
-          newClone.style.backgroundColor = 'lightgreen';
-          newClone.style.transition = 'background-color 0.3s';
-          setTimeout(() => {
-            newClone.style.backgroundColor = '';
-          }, 300);
-        } catch (error) {
-          newClone.style.backgroundColor = 'red';
-          setTimeout(() => {
-            newClone.style.backgroundColor = '';
-          }, 300);
-          console.error(error);
-        }
-      });
-
-      code.textContent = text;
-      if (elementToCopyParent) {
-        elementToCopyParent.insertBefore(newClone, nextSibling);
-      }
-    }
+    code.textContent = text;
+    elementToCopyParent!.insertBefore(newClone, nextSibling);
   }
+}
+
+function copyToClipboard(element: HTMLElement, text: string) {
+  navigator.clipboard
+    .writeText(text)
+    .then(() => highlightElement(element))
+    .catch((error) => {
+      highlightElement(element, error);
+    });
+}
+function createdCopiedMessageElement() {
+  const elm = document.createElement('div');
+  elm.style.position = 'absolute';
+  elm.style.top = '0';
+  elm.style.left = '0';
+  elm.style.width = '100%';
+  elm.style.height = '100%';
+  elm.style.display = 'flex';
+  elm.style.justifyContent = 'center';
+  elm.style.alignItems = 'center';
+  return elm;
+}
+
+function highlightElement(element: HTMLElement, error?: Error) {
+  const elm = createdCopiedMessageElement();
+  if (error) {
+    elm.style.backgroundColor = 'red';
+    elm.textContent = 'Error copying!';
+    console.error(error);
+  } else {
+    elm.style.backgroundColor = 'rgb(0, 255, 0, 0.5)';
+    elm.textContent = 'Copied!';
+  }
+  element.appendChild(elm);
+  setTimeout(() => {
+    element.removeChild(elm);
+    element.style.backgroundColor = '';
+  }, 700);
 }
 
 function getPackageName() {
